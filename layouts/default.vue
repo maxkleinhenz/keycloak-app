8f9c274009e37f35a4ace5a8488ecad081fcbf85
maxkleinhenz <max.github@outlook.de>
2023-07-09 22:14:13 +0200

    display username in app bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,24 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { computedAsync } from '@vueuse/core'
+
+const { status, getSession } = useAuth();
+
+const session = computedAsync(
+  async () => {
+    return await getSession()
+  },
+  null, // initial state
+)
+
+</script>
 
 <template>
   <div class="flex flex-col h-screen">
     <div class="bg-juso-rot p-3 flex flex-row items-center shadow-lg">
       <AppButton to="/" intent="link" shape="default" class="text-white">Gruppenverwaltung</AppButton>
-      <div class="flex justify-end flex-1">
+      <div v-if="status === 'authenticated' && session?.user" class="flex justify-end flex-1">
         <AppButton intent="tertiary" shape="pill" class="text-white">
-          <AppIcon name="ic:baseline-account-circle" /> <span class="hidden sm:inline">Login</span>
+          <AppIcon name="ic:baseline-account-circle" /> <span class="hidden sm:inline">{{ session?.user?.name }}</span>
         </AppButton>
       </div>
     </div>
