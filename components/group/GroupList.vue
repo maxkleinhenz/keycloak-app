32525fe213173c65f97243adae5935d4334b8cc3
maxkleinhenz <max.github@outlook.de>
2023-07-28 23:51:39 +0200

    override primevue styles

--- components/group/GroupList.vue
+++ components/group/GroupList.vue
@@ -1,23 +1,16 @@
 <template>
   <div>
-    <Listbox
-      :options="groups"
-      class="w-full border-none !shadow-none"
-      :pt="{
-        item: { class: '!p-1' },
-      }"
-    >
+    <Listbox :options="groups" class="w-full border-none !shadow-none">
       <template #option="slotProps">
         <component :is="listItem" :class="itemClasses" intent="default">
           <div class="flex-1 text-left">
             {{ slotProps.option.name }}
           </div>
           <div v-if="navigable">
-            <AppIcon
-              name="ic:baseline-chevron-right"
-              class="hover:bg-gray-200 hover:ring-gray-600 hover:ring-1 hover:ring-offset-1 rounded-full"
-            /></div
-        ></component>
+            <AppIcon name="ic:baseline-chevron-right"
+              class="hover:bg-gray-200 hover:ring-gray-600 hover:ring-1 hover:ring-offset-1 rounded-full" />
+          </div>
+        </component>
       </template>
       <template #empty>
         <p>Keine Gruppen</p>
--- pages/profil.vue
+++ pages/profil.vue
@@ -1,50 +1,24 @@
 <template>
   <div class="grid grid-cols-1 mx-auto max-w-5xl p-5 gap-4">
-    <AppPageTitle
-      class
-      :headline="pageHeader"
-      tagline="Profil"
-      :enable-edit="true"
-      v-model:edit="editMode"
-    >
+    <AppPageTitle class :headline="pageHeader" tagline="Profil" :enable-edit="true" v-model:edit="editMode">
     </AppPageTitle>
     <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
       <AppInputField label="Benutzername" class="md:col-span-2">
-        <AppInlineEditableField
-          label="Benutzername"
-          :model-value="profile?.preferred_username"
-          :edit-mode="false"
-        />
+        <AppInlineEditableField label="Benutzername" :model-value="profile?.preferred_username" :edit-mode="false" />
       </AppInputField>
 
       <AppInputField label="Vorname">
-        <AppInlineEditableField
-          label="Vorname"
-          :model-value="profile?.given_name"
-          :edit-mode="editMode"
-        />
+        <AppInlineEditableField label="Vorname" :model-value="profile?.given_name" :edit-mode="editMode" />
       </AppInputField>
       <AppInputField label="Nachname">
-        <AppInlineEditableField
-          label="Nachname"
-          :model-value="profile?.family_name"
-          :edit-mode="editMode"
-        />
+        <AppInlineEditableField label="Nachname" :model-value="profile?.family_name" :edit-mode="editMode" />
       </AppInputField>
 
       <AppInputField label="E-Mail-Adresse">
-        <AppInlineEditableField
-          label="E-Mail-Adresse"
-          :model-value="profile?.email"
-          :edit-mode="false"
-        />
+        <AppInlineEditableField label="E-Mail-Adresse" :model-value="profile?.email" :edit-mode="false" />
       </AppInputField>
       <AppInputField label="E-Mail-Adresse bestätigt">
-        <InputSwitch
-          :model-value="profile?.email_verified"
-          class="scale-90"
-          disabled
-        />
+        <InputSwitch :model-value="profile?.email_verified" class="scale-90" disabled />
       </AppInputField>
     </div>
 
